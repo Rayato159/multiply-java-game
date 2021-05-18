72ca6843a4b867e794b324e41c0263ded02a98b8
Rayato159 <Rayato159@gmail.com>
2021-05-19 06:43:31 +0700

    try catch added

--- src/Multiply/Main.java
+++ src/Multiply/Main.java
@@ -7,23 +7,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.println("CreateBy Ruangyot Nanchiang\n");
         while(true) {
             Problem problem = new Problem();
             problem.getProblem();
 
-            System.out.print("Answer = ");
-            int user_answer = sc.nextInt();
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Answer = ");
+                int user_answer = sc.nextInt();
 
-            if(problem.logic(user_answer)){
-                System.out.println("Collect!");
+                if(problem.logic(user_answer)){
+                    System.out.println("Collect!");
+                }
+                else{
+                    System.out.println("Game Over. Score: " + problem.getScore());
+                    break;
+                }
+                System.out.println();
             }
-            else{
-                System.out.println("Game Over. Score: " + problem.getScore());
-                break;
+
+            catch(Exception e) {
+                System.out.println("Error! Please enter integer number.\n");
             }
-            System.out.println();
+
         }
     }
 }
